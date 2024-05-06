7e8d56ee5b98a70fea51461d877b2fee171a46e7
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-05-06 23:08:27 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+        myLinkedList.reverse();
+        myLinkedList.show();
+
+
+
+
+    }
+
+}
